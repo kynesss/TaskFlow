3ab75e5fbd704d446541c3f212568bac223957da
Kynes <kamilpirog98@gmail.com>
2025-04-17 15:08:03 +0200

    feat: handle admin seed

--- TaskFlow.Infrastructure/Persistence/IdentitySeeder.cs
+++ TaskFlow.Infrastructure/Persistence/IdentitySeeder.cs
@@ -6,10 +6,12 @@ namespace TaskFlow.Infrastructure.Persistence
     public class IdentitySeeder
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IdentitySeeder(RoleManager<IdentityRole> roleManager)
+        public IdentitySeeder(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public async Task SeedAsync()
@@ -17,6 +19,19 @@ namespace TaskFlow.Infrastructure.Persistence
             await CreateRoleIfNotExistsAsync(Roles.Admin);
             await CreateRoleIfNotExistsAsync(Roles.Manager);
             await CreateRoleIfNotExistsAsync(Roles.User);
+
+            var adminExists = await _userManager.FindByEmailAsync("admin@taskflow.com") != null;
+            if (adminExists)
+                return;
+
+            var admin = new IdentityUser()
+            {
+                UserName = "Admin",
+                Email = "admin@taskflow.com"
+            };
+
+            await _userManager.CreateAsync(admin, "Admin123!");
+            await _userManager.AddToRoleAsync(admin, Roles.Admin);
         }
 
         private async Task CreateRoleIfNotExistsAsync(string roleName)
