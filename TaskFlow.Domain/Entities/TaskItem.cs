f4ca3aba7ac8523e6ffa3e34990f84bc8cc92734
Kynes <kamilpirog98@gmail.com>
2025-04-17 19:50:07 +0200

    feat: handle basic task item service

--- TaskFlow.Domain/Entities/TaskItem.cs
+++ TaskFlow.Domain/Entities/TaskItem.cs
@@ -9,7 +9,7 @@ namespace TaskFlow.Domain.Entities
         public string? Description { get; set; }
         public TaskPriority Priority { get; set; }
         public Enums.TaskStatus Status { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string CreatedBy { get; set; } = default!;
         public string? AssignedTo { get; set; }
     }
---
+++ TaskFlow.Infrastructure/Services/TaskItemService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Application.ApplicationUser;
+using TaskFlow.Application.DTOs;
+using TaskFlow.Application.Interfaces;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Infrastructure.Persistence;
+
+namespace TaskFlow.Infrastructure.Services
+{
+    public class TaskItemService : ITaskItemService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IUserContext _userContext;
+
+        public TaskItemService(ApplicationDbContext dbContext, IMapper mapper, IUserContext userContext)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _userContext = userContext;
+        }
+
+        public async Task<int> CreateAsync(TaskItemDto dto)
+        {
+            var item = _mapper.Map<TaskItem>(dto);
+
+            item.CreatedAt = DateTime.UtcNow;
+            item.CreatedBy = _userContext.GetCurrentUser()!.Id;
+
+            _dbContext.TaskItems.Add(item);
+            await _dbContext.SaveChangesAsync();
+
+            return item.Id;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var item = await _dbContext.TaskItems.FirstAsync(x => x.Id == id);
+            _dbContext.TaskItems.Remove(item);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TaskItemDto>> GetAllAsync()
+        {
+            var items = await _dbContext.TaskItems.ToListAsync();
+            var dtos = _mapper.Map<IEnumerable<TaskItemDto>>(items);
+
+            return dtos;
+        }
+
+        public async Task<TaskItemDto> GetByIdAsync(int id)
+        {
+            var item = await _dbContext.TaskItems.FirstOrDefaultAsync(x => x.Id == id) ?? throw new NullReferenceException($"TaskItem with ID {id} not found.");
+            var dto = _mapper.Map<TaskItemDto>(item);
+            return dto;
+        }
+
+        public async Task UpdateAsync(TaskItemDto dto)
+        {
+            var entity = await _dbContext.TaskItems.FirstAsync(x => x.Id == dto.Id);
+
+            entity.Description = dto.Description;
+            entity.AssignedTo = dto.AssignedTo;
+            entity.Title = dto.Title;
+            entity.Status = dto.Status;
+            entity.Priority = dto.Priority;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
